4e6997869c9002485529e2a372e71cc3577da0a0
Benoit Franck <benoitfranck201@yahoo.fr>
2019-01-29 10:05:24 +0100

    added counter

--- js/request.js
+++ js/request.js
@@ -4,12 +4,16 @@ let robots = new Vue({
     el:"#robots",
     data:{
         robots:[],
-        filter: "all"
+        filter: "all",
+        males:0,
+        females:0
     },
     created(){
         axios.get("https://wt-902485dbb4fca4fccee3a0efcde5b34c-0.sandbox.auth0-extend.com/robots")
         .then(response => {
             this.robots = response.data;
+            this.males = this.robots.filter(robot => robot.gender === "Male").length;
+            this.females = this.robots.filter(robot => robot.gender === "Female").length;
         })
     },
     methods: {
